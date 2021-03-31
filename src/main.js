b65127f7fd01542fbf9c15a3d1ebc367ba45bee8
imcris <zanardy20@hotmail.com>
2021-03-31 10:56:57 -0500

    add envs

--- src/main.js
+++ src/main.js
@@ -1,10 +1,26 @@
 import Vue from 'vue'
+import firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/firestore";
+import "firebase/storage";
+import "firebase/auth";
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_API_KEY_FIREBASE,
+  authDomain: process.env.VUE_APP_AUTHDOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASEURL,
+  projectId: process.env.VUE_APP_PROJECTID,
+  storageBucket: process.env.VUE_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGINGSENDERID,
+  appId: process.env.VUE_APP_APPID,
+  measurementId: process.env.VUE_APP_MEASUREMENTID
+};
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
